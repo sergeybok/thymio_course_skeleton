83b80cb38ea987253db057ff10be54114aec6e88
Sergey Bokhnyak <sergeybok@gmail.com>
2018-05-30 20:59:48 +0200

    850 cnnout, 8 rnnhid, step500 0.045, step 980 0.029, test old way 0.39 right way 0.15

--- scripts/rnn_train.py
+++ scripts/rnn_train.py
@@ -11,7 +11,7 @@ b_size = 40
 stack = 2
 
 max_t = 6
-rnn_hid = 16
+rnn_hid = 8
 
 def rnn_batchdata(dx,dy,mt,bs,b,h,w,c):
     # dx: dataset_x, dy: dataset_y
@@ -33,8 +33,8 @@ def CNN(x):
     #small_x = tf.image.resize_images(x,[64,64])
     small_x = x
     conv1 = tf.layers.conv2d(small_x,filters=16,kernel_size=[7,7],padding='valid',activation=tf.nn.relu)
-    pool1 = tf.layers.max_pooling2d(conv1,pool_size=[4,4],strides=[4,4])
-    conv2 = tf.layers.conv2d(pool1, filters=8,kernel_size=[4,4],padding='valid',activation=tf.nn.relu)
+    pool1 = tf.layers.max_pooling2d(conv1,pool_size=[2,2],strides=[2,2])
+    conv2 = tf.layers.conv2d(pool1, filters=6,kernel_size=[4,4],padding='valid',activation=tf.nn.relu)
     pool2 = tf.layers.max_pooling2d(conv2,pool_size=[3,3],strides=[3,3])
     #conv3 = tf.layers.conv2d(pool2,filters=8, kernel_size=[3,3],padding='valid')
     #pool3 = tf.layers.max_pooling2d(conv3,pool_size=[2,2],strides=[2,2])
@@ -86,7 +86,7 @@ rprediction = tf.nn.tanh(rdense)
 
 rnn_Error = tf.losses.mean_squared_error(labels=Ry,predictions=rprediction)
 roptimizer = tf.train.AdamOptimizer(learning_rate=lr,beta1=0.99,beta2=0.9999)
-train_rnn = optimizer.minimize(rnn_Error)
+train_rnn = roptimizer.minimize(rnn_Error)
 
 
 sess = tf.InteractiveSession()
@@ -145,7 +145,7 @@ def test_model(inputx=Rx,inputy=Ry,error=rnn_Error,steps=20,testdir='test'):
     testy = testy.reshape((-1,1))
     testx /= 255.0 
     avg = 0
-    for i in range(steps):
+    for s in range(steps):
         bx, by = rnn_batchdata(testx,testy,max_t,b_size,s,84,84,stack)
         e = sess.run(error,{inputx:bx,inputy:by})
         print(e)
